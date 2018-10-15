d38fb842babcfe748e536264b1f215aa7336c25c
dshowing <watt109629@outlook.com>
2018-10-15 23:46:36 +0800

    playing user list

--- mellplayer/api.py
+++ mellplayer/api.py
@@ -44,13 +44,13 @@ class Netease(object):
         return qrcode
 
 
-    def _request(self, url, payload, method='GET', is_raw=True, data=None):
+    def _request(self, url, payload=None, method='GET', is_raw=True, data=None):
         '''
         对requests简单封装
         '''
         headers = {'appver': '2.0.2', 'Referer': 'http://music.163.com'}
         if method == 'GET':
-            result = requests.get(url=url, headers=headers, params=payload)
+            result = requests.get(url=url, headers=headers, params=payload)   #添加了params参数，通过payload方式请求
         elif method == 'POST' and data:
             result = requests.post(url=url, data=data, headers=headers)
         # if request failed, return False
@@ -64,6 +64,7 @@ class Netease(object):
 #######################################
 #    用户登陆后的请求信息
 #######################################
+
     def user_id(self):
         '''
         用户ID
@@ -82,34 +83,94 @@ class Netease(object):
         result = self._request(url)
         return result
 
-    def user_playlist_categories(self, user_id):
+    def user_playlist(self, user_id):
         '''
         用户歌单（创建）
-        https://music.163.com/#/my/m/music/playlist
+        http://music.163.com/#/my/m/music/playlist?id=30408531
         {id: 30408531}
         '''
-        payload = {id: user_id}
-        url = 'https://music.163.com/#/my/m/music/playlist'
+        payload = {'id': 'user_id'}
+        url = 'http://music.163.com/#/my/m/music/playlist'
         result = self._request(url, payload)
         return result
 
-    def
+    def user_playlist_info(self, list_id):
+        '''
+        用户歌单详情
+        http://music.163.com/#/my/m/music/playlist?id=30408531
+        '''
+        payload = {'id': 'list_id'}
+        url = 'http://music.163.com/#/my/m/music/playlist'
+        result = self._request(url, payload)
+        return result
+
+    def user_song_detail(self, song_id):
+        '''
+        歌曲详情
+        https://music.163.com/#/song?id=254460
+        https://music.163.com/#/song?id=1311319058
+        '''
+        payload = {'id': 'song_id'}
+        url = 'https://music.163.com/#/song'
+        result = self._request(url, payload)
+        return result
+
+    def user_lyric_detail(self, song_id):
+        '''
+        歌词详情
+        http://music.163.com/api/song/lyric?os=osx&id=xxx&lv=-1&kv=-1&tv=-1
+        使用mell作者api，不清楚怎么找到的？
+        '''
+        payload = {'lyric': 'song_id', 'os': 'osx', 'id':song_id, 'lv':'-1', 'kv':'-1', 'tv':'-1'}
+        url = 'http://music.163.com/api/song'
+        result = self._request(url, payload)
+        return result
+
+    def user_result_parse(self, data, parse_type):
+        '''
+        解析信息
+        '''
+        res = None
+        if parse_type == 'category_playlists':
+            res = [d['id'] for d in data['playlists']]
+
+        # 播放列表
+        elif parse_type == 'playlist_detail':
+            tracks = data['result']['tracks']
+            playlist_ids = [t['id'] for t in tracks]
+            playlist_detail = {t['id']: {
+                'song_id': t['id'],
+                'song_name': t['name'],
+                'song_url': t['mp3Url'],
+                'song_artists': ' & '.join(map(lambda a: a['name'], t['artists']))
+            } for t in tracks}
+            res = (playlist_ids, playlist_detail)
+
+        # 歌词信息
+        elif parse_type == 'lyric_detail':
+            if 'lrc' in data:
+                res = {
+                    'lyric': data['lrc']['lyric']
+                }
+            else:
+                res = {
+                    'lyric': 'no_lyric'
+                }
+
+        # 歌曲详情
+        elif parse_type == 'song_detail_new':
+            res = {d['id']: {
+                'song_url': d['url'],
+                'song_br': d['br']
+            } for d in data['data']}
+
+        return res
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+###############################################
+#     游客歌单
+###############################################
 
     def playlist_categories(self):
         '''
